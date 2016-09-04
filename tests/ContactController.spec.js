0adb72450496b4c4682c34208bce8e1593a15db9
antoinfive <antoinfive@gmail.com>
2016-09-04 12:06:35 -0400

    Done.

--- tests/ContactController.spec.js
+++ tests/ContactController.spec.js
@@ -6,5 +6,24 @@ describe('ContactController', function () {
     beforeEach(inject(function (_$controller_) {
         $controller = _$controller_;
     }));
+		
+		it('should re-filter the results correctly and return all females', function() {
+						var $scope = {};
+						$controller('ContactController as vm', {$scope: $scope});
+			
+						$scope.vm.search = 'female' 						
+						$scope.vm.changeFilter();
 
+		expect($scope.vm.filteredList.length).toEqual(71);				
+		});
+
+		it('should re-filter the results correctly and return all members from manchester', function() {
+						var $scope = {}
+						$controller('ContactController as vm', {$scope: $scope});
+						
+						$scope.vm.search = 'manchester'
+            $scope.vm.changeFilter();
+
+		expect($scope.vm.filteredList.length).toEqual(8);
+		});	
 });
