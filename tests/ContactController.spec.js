a5a4fddfe825cb746d939942364a051e65469723
thecarsgone <carolina.nicole.gonzalez@gmail.com>
2016-09-09 13:35:48 -0400

    Done

--- tests/ContactController.spec.js
+++ tests/ContactController.spec.js
@@ -7,4 +7,30 @@ describe('ContactController', function () {
         $controller = _$controller_;
     }));
 
+    it('returns all female contacts', function () {
+      var $scope = {};
+      $controller('ContactController as vm', {$scope: $scope});
+
+      $scope.vm.search = "female";
+
+      $scope.vm.changeFilter();
+
+      expect($scope.vm.filteredList.length).toEqual(71);
+
+    });
+
+    it('selects people from different geographical locations', function () {
+      var $scope = {};
+      $controller('ContactController as vm', {$scope: $scope});
+
+      $scope.vm.search = "manchester";
+
+      $scope.vm.changeFilter();
+
+      expect($scope.vm.filteredList.length).toEqual(8);
+
+
+
+    });
+
 });
